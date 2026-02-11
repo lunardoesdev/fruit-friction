0d49aa4511b19d662f8953f487cd36b7450289b3
Lunar <lunarlifeburst@gmail.com>
2026-02-12 02:09:56 +1000

    multifruit

--- index.ts
+++ index.ts
@@ -30,7 +30,107 @@ class Sphere extends ex.Actor {
   }
 }
 
-let sphere = new Sphere(42);
+enum FruitKind {
+  Cherry,
+  Papaya,
+  Mango,
+  Peach,
+  Melon,
+  Watermelon,
+  Nothing = 999,
+}
+
+class Fruit extends ex.Actor {
+  falling: boolean;
+  kind: FruitKind;
+
+  override onPreCollisionResolve(
+    self: ex.Collider,
+    other: ex.Collider,
+    side: ex.Side,
+    contact: ex.CollisionContact,
+  ): void {
+    const workkind = (self.owner as Fruit).kind;
+
+    if (workkind == FruitKind.Nothing) {
+      return;
+    }
+
+    if (workkind == FruitKind.Watermelon) {
+      // biggest one
+      return;
+    }
+
+    if ((self.owner as Fruit).kind == (other.owner as Fruit).kind) {
+      (self.owner as Fruit).kind = FruitKind.Nothing;
+      (other.owner as Fruit).kind = FruitKind.Nothing;
+
+      engine.remove(self.owner as ex.Actor);
+      engine.remove(other.owner as ex.Actor);
+
+      const f = new Fruit(workkind + 1);
+      f.pos = contact.points[0] || ex.vec(0, 0);
+      f.falling = true;
+      f.body.collisionType = ex.CollisionType.Active;
+      engine.add(f);
+    }
+  }
+
+  constructor(kind: FruitKind) {
+    let radius = 0;
+    let color = new ex.Color(255, 0, 0);
+
+    switch (kind) {
+      case FruitKind.Cherry: {
+        radius = 10;
+        color = new ex.Color(255, 0, 0);
+        break;
+      }
+      case FruitKind.Papaya: {
+        radius = 20;
+        color = new ex.Color(255, 255, 0);
+        break;
+      }
+      case FruitKind.Peach: {
+        radius = 30;
+        color = new ex.Color(255, 0, 255);
+        break;
+      }
+      case FruitKind.Mango: {
+        radius = 40;
+        color = new ex.Color(100, 100, 0);
+        break;
+      }
+      case FruitKind.Melon: {
+        radius = 50;
+        color = new ex.Color(100, 0, 100);
+        break;
+      }
+      case FruitKind.Watermelon: {
+        radius = 60;
+        color = new ex.Color(50, 0, 200);
+        break;
+      }
+    }
+
+    super({
+      pos: ex.vec(engine.input.pointers.primary.lastWorldPos.x, 2 * radius),
+      collisionType: ex.CollisionType.PreventCollision,
+    });
+    this.kind = kind;
+    this.collider.useCircleCollider(radius);
+    this.graphics.use(
+      new ex.Circle({
+        radius: radius,
+      }),
+    );
+
+    this.color = color;
+    this.falling = false;
+  }
+}
+
+let sphere = new Fruit(FruitKind.Cherry);
 
 const floorHeight = 10;
 const floor = new ex.Actor({
@@ -67,7 +167,7 @@ engine.input.pointers.primary.on("up", function (evt) {
   if (!sphere.falling) {
     sphere.falling = true;
     sphere.body.collisionType = ex.CollisionType.Active;
-    sphere = new Sphere(42);
+    sphere = new Fruit(Math.round(Math.random() * FruitKind.Watermelon));
     engine.add(sphere);
   }
 });
