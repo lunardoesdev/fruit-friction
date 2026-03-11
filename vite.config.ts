0fa1182bfbe6b3ad91048a3a2638b6b96ba12f9a
Lunar <lunarlifeburst@gmail.com>
2026-03-11 21:19:03 +1000

    github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,14 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig(() => {
+  const repository = process.env.GITHUB_REPOSITORY?.split("/")[1];
+  const owner = process.env.GITHUB_REPOSITORY?.split("/")[0];
+  const isUserOrOrgPagesRepo = repository === `${owner}.github.io`;
+
+  return {
+    base:
+      process.env.GITHUB_ACTIONS && repository && !isUserOrOrgPagesRepo
+        ? `/${repository}/`
+        : "/",
+  };
+});
